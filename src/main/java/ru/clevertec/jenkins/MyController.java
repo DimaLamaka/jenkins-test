cece7aea084b709ee1cb739cab3fd8ebed10de8d
dzmitry lamaka <lamaka.dima@gmail.com>
2022-04-08 11:45:48 +0300

    Initial commit

---
+++ src/main/java/ru/clevertec/jenkins/MyController.java
@@ -0,0 +1,14 @@
+package ru.clevertec.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jenkins-api/v1")
+public class MyController {
+    @GetMapping
+    public String hello() {
+        return "Hello from Jenkins";
+    }
+}
---
+++ src/test/java/ru/clevertec/jenkins/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,25 @@
+package ru.clevertec.jenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test(){
+        Assertions.assertEquals(2,1+1);
+    }
+
+    @Test
+    void test2(){
+        Assertions.assertEquals(2,2);
+    }
+
+}
