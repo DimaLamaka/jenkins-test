95efbf528bb3f95a51dbbf8463ab75af4acc87d4
dzmitry lamaka <lamaka.dima@gmail.com>
2022-04-08 15:56:24 +0300

    fix jenkins file

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,5 @@
 pipeline{
-  agent any
+  agent { docker { image 'amazoncorretto:latest' } }
   stages{
    stage('Clone git'){
     steps{
--- src/test/java/ru/clevertec/jenkins/SpringbootJenkinsApplicationTests.java
+++ src/test/java/ru/clevertec/jenkins/SpringbootJenkinsApplicationTests.java
@@ -3,7 +3,6 @@ package ru.clevertec.jenkins;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringbootJenkinsApplicationTests {
